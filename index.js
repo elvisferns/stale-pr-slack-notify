8d39cfbc2c3caa7890ce285e0f0c8e6927526204
elvis <elvisfernandes.fernandes@kaplan.com>
2021-10-07 16:32:57 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// import { axios } from "axios";
+
+// const pullStalePRs = () => {
+
+// }
+
+// const notifySlack = () => {
+  
+// }
+
+const run = () => {
+  console.log('env variables', process.env)
+}
+
+run();
