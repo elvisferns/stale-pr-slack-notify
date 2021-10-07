a6363ad40f031f062a0b9b403df9668130e8fd3f
elvis <elvisfernandes.fernandes@kaplan.com>
2021-10-07 19:09:06 +0530

    update pullStalePRs func

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 
 import { default as axios } from "axios";
 
-// const pullStalePRs = () => {
+const pullStalePRs = async (repo, token) => {
+  const queryParams = `q=is:pr repo:${repo} state:open`;
+  const res = await axios.get(`https://api.github.com/search/issues?${queryParams}`, {
+    headers: {
+      Accept: "application/vnd.github.v3.raw+json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-// }
+  console.log("file: index.js ~ line 18 ~ run ~ res", res.data);
+};
 
 // const notifySlack = () => {
 
@@ -12,15 +20,8 @@ import { default as axios } from "axios";
 
 const run = async () => {
   console.log("env variables", process.env);
-  const queryParams = `q=is:pr repo:${process.env.GITHUB_REPOSITORY} state:open`;
   try {
-    const res = await axios.get(`https://api.github.com/search/issues?${queryParams}`, {
-      headers: {
-        Accept: "application/vnd.github.v3.raw+json",
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      },
-    });
-    console.log("file: index.js ~ line 18 ~ run ~ res", res);
+    await pullStalePRs(process.env.GITHUB_REPOSITORY, process.env.GITHUB_TOKEN);
   } catch (error) {
     console.error(error.message);
     process.exit(1);
