8a1b0df4437a0e81772bd326990d5dd05a01ed27
elvis <elvisfernandes.fernandes@kaplan.com>
2021-10-07 18:53:25 +0530

    add github pull request

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 // import { axios } from "axios";
 
+import { default as axios } from "axios";
+
 // const pullStalePRs = () => {
 
 // }
 
 // const notifySlack = () => {
-  
+
 // }
 
-const run = () => {
-  console.log('env variables', process.env)
-}
+const run = async () => {
+  console.log("env variables", process.env);
+  const queryParams = `q=is:pr repo:${process.env.GITHUB_REPOSITORY} state:open`;
+  try {
+    const res = await axios.get(`https://api.github.com/search/issues?${queryParams}`, {
+      headers: {
+        Accept: "application/vnd.github.v3.raw+json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      },
+    });
+    console.log("file: index.js ~ line 18 ~ run ~ res", res);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 run();
